d917ad39f692a66dab8352986790f2d147b48384
Alan Zhu <90356452+alanzhu1@users.noreply.github.com>
2022-01-24 16:03:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,48 +3,48 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-
 // URLs
 app.use(express.static('public'));
+global.path='';
 
-
-
-
-app.set('subdomain offset', 1); // 1 for local, 2 for example.com, 3 for econverse.herokuapp.com, 4 for econverse.sites.tjhsst.edu, ...
 app.get('*', function(req, res, next) {
-    if(req.subdomains.length===0){
+    path = req.originalUrl;
+    var date = String(new Date()).substring(0,24) +" ET";
+    console.log(date + " | Request through  " + path)
+    if(path=='/'){
         res.render('index.ejs');
-        console.log('a');
+        console.log(date + " |      - Rendered     index.ejs")
     }
     else {
         res.render('room.ejs');
+        console.log(date + " |      - Rendered     room.ejs")
     }
     next();
 });
 
-
 // Socket events
 io.sockets.on('connection', function(socket) {
+    var confirmedPath="␜" + path + "␜";
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('on', '🟩&#11166;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + socket.username + '</strong> <i>joined the session.</i>');
+        io.emit('on', confirmedPath + '🟩&#11166;&nbsp;&nbsp;&nbsp;&nbsp;<b>' + socket.username + '</b> <i>joined the session.</i>');
         
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('off', '🟥&#11164;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + socket.username + '</strong> <i>left the session.</i>');
+        io.emit('off', confirmedPath + '🟥&#11164;&nbsp;&nbsp;&nbsp;&nbsp;<b>' + socket.username + '</b> <i>left the session.</i>');
         
     })
     //🟦
     socket.on('msg', function(message) {
-        io.emit('message', '<strong>' + socket.username + '</strong>&nbsp;&nbsp;&nbsp;' + message);
+        io.emit('message', confirmedPath + '<b>' + socket.username + '</b>&nbsp;&nbsp;&nbsp;' + message);
         
     });
 
 });
 
-const date = String(new Date()).substring(0,24) +" ET";
 // Server
 const server = http.listen(process.env.PORT || 80, function() { 
+    var date = String(new Date()).substring(0,24) +" ET";
     console.log( date + ' | Server started.');
-}); 
+});
